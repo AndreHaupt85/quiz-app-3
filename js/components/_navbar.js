95dbf0cc92c2bd0263b06c28477c760ff9166922
Andre Haupt <andrehaupt@me.com>
2022-08-16 13:03:36 +0200

    create animation js

--- js/components/_navbar.js
+++ js/components/_navbar.js
@@ -9,11 +9,20 @@ export default function navigation() {
     const navAddpage = document.querySelector('[data-js="nav-addpage"]');
     const navProfile = document.querySelector('[data-js="nav-profile"]');
 
+    const quizIndicators = document.querySelector('[data-js="nav-Quizcard"]');
+    const bookmarkIndicators = document.querySelector('[data-js="nav-Quizcard"]');
+    const addPageIndicators = document.querySelector('[data-js="nav-Quizcard"]');
+    const profileIndicators = document.querySelector('[data-js="nav-Quizcard"]');
+
     navQuizcard.addEventListener("click", () => {
         homePage.classList.remove("hidden");
         bookmarkPage.classList.add("hidden");
         addPage.classList.add("hidden");
         profilePage.classList.add("hidden");
+        quizIndicators.classList.add("blink_me");
+        bookmarkIndicators.classList.remove("blink_me");
+        addPageIndicators.classList.remove("blink_me");
+        profileIndicators.classList.remove("blink_me");
     });
 
     navBookmarks.addEventListener("click", () => {
